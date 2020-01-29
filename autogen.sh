fc7f89d35fb6561dca5fe558709ac807d462ac1e
persianpros <persianpros@yahoo.com>
2020-01-29 11:54:41 +0330

    Get rid of ipkg name once for all, we must use opkg name to not confuse anyone

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@
 autoreconf --force --install
 
 echo "now, execute ./configure"
-echo "after that, you could build an ipkg with 'make ipkg' in any of the"
-echo "directories which support the ipkg target."
+echo "after that, you could build an opkg with 'make opkg' in any of the"
+echo "directories which support the opkg target."
